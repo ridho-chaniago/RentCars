2c4ee121d16fdd6ef092bc9b9991ffe4e4e92b8b
Ridho-Chaniago <ridho12c.rc@gmail.com>
2024-05-19 15:36:31 +0700

    auto deploy

--- rentcar/src/component/molecules/BtnIcon.jsx
+++ rentcar/src/component/molecules/BtnIcon.jsx
@@ -3,25 +3,25 @@ import React from 'react'
 export const BtnUser = () => {
     return (
         <button>
-            <img width={"30px"} src="public/images/user.png" alt="" />
+            <img width={"30px"} src="images/user.png" alt="" />
         </button>
     )
 }
 export const BtnFav = () => {
     return (
         <button>
-            <img width={"30px"} src="/images/fav.png" alt="" />
+            <img width={"30px"} src="images/fav.png" alt="" />
         </button>
     )
 }
 export const BtnSearch = () => {
     return (
         <button className='absolute right-4'>
-            <img width={"15px"} src="/images/searchIcon.png" alt="" />
+            <img width={"15px"} src="images/searchIcon.png" alt="" />
         </button>
     )
 }
-export const BtnNewSec = ({children}) => {
+export const BtnNewSec = ({ children }) => {
     return (
         <button className='bg-[#AF1313] text-white w-[40%] lg:p-4 lg:text-5xl p-1'>{children}</button>
     )
