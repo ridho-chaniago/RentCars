c8fa337c020f9d7cb0778047b00c5ea551a8b216
Ridho-Chaniago <ridho12c.rc@gmail.com>
2024-05-19 16:02:14 +0700

    auto deploy

--- rentcar/src/component/template/Footer.jsx
+++ rentcar/src/component/template/Footer.jsx
@@ -4,9 +4,9 @@ import Logo from '../atom/logo'
 function Footer() {
     return (
         <div className='p-2 space-y-4 md:flex md:justify-between bg-[#F2F2F2]'>
-            <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-4 w-[40%]'>
                 <Logo />
-                <p>is a web-based application that sells various types of car brands.<br/> This application was created to make it easier for<br/>buyers so that buyers don't have to visit the place directly<br/> to see the car, just go through the website to see the details of the<br/> car and also with more flexible times.</p>
+                <p>is a web-based application that sells various types of car brands. This application was created to make it easier for buyers so that buyers don't have to visit the place directly to see the car, just go through the website to see the details of the car and also with more flexible times.</p>
                 <div className='flex gap-3'>
                     <img className='w-6' src="/images/phone.png" alt="" />
                     <p className='font-medium'>Ridho 0812-7678-4668</p>
@@ -17,7 +17,7 @@ function Footer() {
                     <img className='w-6' src="/images/location.png" alt="" />
                     <p className='font-medium'>Find our location</p>
                 </div>
-                <p>Jl. Ahmad Yani, Tlk. Tering, Kec. Batam Kota,
+                <p>Jl. Ahmad Yani, Tlk. Tering, Kec. Batam Kota,<br/>
                     Kota Batam, Kepulauan Riau 29461</p>
             </div>
         </div>
