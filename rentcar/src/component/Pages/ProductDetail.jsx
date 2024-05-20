cbb8ccefda607a20de3ae9f4c07f65dfc7c06859
Ridho-Chaniago <ridho12c.rc@gmail.com>
2024-05-20 10:13:58 +0700

    auto deploy

--- rentcar/src/component/Pages/ProductDetail.jsx
+++ rentcar/src/component/Pages/ProductDetail.jsx
@@ -23,11 +23,12 @@ const ProductDetail = ({ product }) => {
 
             </div>
             <div className="product-detail p-4 ">
-                    <div className='flex flex-col items-center' >
+                <div className='md:flex '>
+                    <div className='flex flex-col items-center md:w-[60%] ' >
                         <h1 className='font-bold text-[35px] '>{detail.title}</h1>
-                        <img className='w-full' src={detail.image} alt={detail.title} />
+                        <img className='w-full ' src={detail.image} alt={detail.title} />
                     </div>
-                    <div className='space-y-2'>
+                    <div className='space-y-2 md:pt-16' >
                         <p className='font-bold'>{detail.title} {detail.title2}</p>
                         <p>STNK dan BPKB asli & sesuai*<br />
                             Pajak : 2024-07-01<br />
@@ -38,20 +39,21 @@ const ProductDetail = ({ product }) => {
                             Warna : Abu-Abu</p>
                         <p className='font-bold text-2xl'>{detail.price}</p>
                     </div>
-                    <div className='mt-3'>
-                        <div className="font-thin">
-                            <p>All Inclusive Price</p>
-                            <ul className='list-disc list-inside'>
-                                <li>1-Year Warranty (Engine, Transmission, AC) Learn More</li>
-                                <li>Road Tax</li>
-                                <li>1 Year TLO Insurance</li>
-                            </ul>
-                        </div>
-                        <div className='flex justify-around py-4'>
-                            <BtnNewSec className={"rounded-full font-bold"}>Beli</BtnNewSec>
-                            <BtnAddFav/>
-                        </div>
+                </div>
+                <div className='mt-3 md:flex'>
+                    <div className="font-thin md:w-[60%]">
+                        <p>All Inclusive Price</p>
+                        <ul className='list-disc list-inside'>
+                            <li>1-Year Warranty (Engine, Transmission, AC) Learn More</li>
+                            <li>Road Tax</li>
+                            <li>1 Year TLO Insurance</li>
+                        </ul>
                     </div>
+                    <div className='flex justify-around py-4 md:pr-4 md:w-[40%] md:gap-8'>
+                        <BtnNewSec className={"rounded-full md:text-2xl font-bold md:w-full"}>Beli</BtnNewSec>
+                        <BtnAddFav />
+                    </div>
+                </div>
             </div>
         </>
     );
